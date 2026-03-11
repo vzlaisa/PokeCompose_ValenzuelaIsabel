268b9f53e88acb91e6b6a57a4721b4dae9fe7ac2
vzlaisa <147114185+vzlaisa@users.noreply.github.com>
2026-03-10 17:44:15 -0700

    Validación en inicio de sesión

--- app/src/main/java/valenzuela/isabel/pokecompose_valenzuelaisabel/ui/screens/LoginScreen.kt
+++ app/src/main/java/valenzuela/isabel/pokecompose_valenzuelaisabel/ui/screens/LoginScreen.kt
@@ -1,6 +1,8 @@
 package valenzuela.isabel.pokecompose_valenzuelaisabel.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -9,7 +11,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -20,8 +24,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,10 +47,18 @@ fun LoginScreen(
         mutableStateOf("")
     }
 
+    // Flags para error
+    var errorUsuario by remember {
+        mutableStateOf(false)
+    }
+    var errorPassword by remember {
+        mutableStateOf(false)
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(15.dp),
+            .padding(30.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
 
@@ -59,47 +73,84 @@ fun LoginScreen(
                 .padding(10.dp)
         )
 
+        Spacer(
+            modifier = Modifier.height(15.dp)
+        )
+
         Text(
             text = "Inicio de sesión",
-            style = MaterialTheme.typography.headlineLarge
+            style = MaterialTheme.typography.headlineLarge.copy(
+                fontFamily = FontFamily.Serif
+            )
         )
 
         Spacer(
-            modifier = Modifier.height(15.dp)
+            modifier = Modifier.height(20.dp)
         ) // Espacio con altura
 
+        // Campo de username
         OutlinedTextField(
             value = username,
-            onValueChange = {username = it},
+            onValueChange = {
+                username = it
+                errorUsuario = false},
             label = { // placeholder
                 Text(text = "Nombre de usuario")
             },
-            modifier = Modifier.fillMaxWidth()
+            isError = errorUsuario,
+            supportingText = {
+                if (errorUsuario) {
+                    Text("El usuario no puede estar vacío")
+                }
+            },
+            shape = RoundedCornerShape(15.dp),
+            modifier = Modifier
+                .fillMaxWidth()
         )
 
         Spacer(
-            modifier = Modifier.height(6.dp)
+            modifier = Modifier.height(4.dp)
         )
 
+        // Campo de contraseña
         OutlinedTextField(
             value = password,
-            onValueChange = {password = it},
+            onValueChange = {
+                password = it
+                errorPassword = false},
+            isError = errorPassword,
+            supportingText = {
+                if (errorPassword) {
+                    Text("La contraseña no puede estar vacía")
+                }
+            },
             label = { // placeholder
                 Text(text = "Contraseña")
             },
-            modifier = Modifier.fillMaxWidth()
+            shape = RoundedCornerShape(15.dp),
+            modifier = Modifier
+                .fillMaxWidth()
         )
 
         Spacer(
-            modifier = Modifier.height(15.dp)
+            modifier = Modifier.height(20.dp)
         )
 
         // Botón para iniciar sesión
         Button(
             onClick = {
-                onLoginSuccess() // Usar la función recibida
+                errorUsuario = username.isBlank()
+                errorPassword = password.isBlank()
+
+                if (!errorUsuario && !errorPassword) {
+                    onLoginSuccess()
+                }
             },
-            modifier = Modifier.fillMaxWidth()
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFBE7EA8)
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
         ) {
             Text(text = "Iniciar")
         }
